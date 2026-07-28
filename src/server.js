7823268a3b3b14e5089a6360dd24f94cf50af0cd
Oleksandra Starovatova <starovatovasasha@gmail.com>
2026-07-28 13:16:51 +0300

    error middlewares, logger

--- src/server.js
+++ src/server.js
@@ -1,38 +1,29 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import pino from 'pino-http';
+import { errors  } from "celebrate";
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { User } from "./models/user.js";
 import { Item } from "./models/item.js";
 import { Wishlist } from "./models/wishlist.js";
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
+
 app.use(express.json());
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger);
+
 app.post('/users', async (req, res) => {
   try {
-    const newContact = await User.create(req.body);
+    const newUser = await User.create(req.body);
     res.status(201).json({
       status: 201,
       message: 'User successfully created!',
-      data: newContact,
+      data: newUser,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -63,6 +54,11 @@ app.post('/wishlist', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+app.use(notFoundHandler);
+app.use(errors());
+app.use(errorHandler);
+
 await connectMongoDB();
 
 app.listen(PORT, () => {
